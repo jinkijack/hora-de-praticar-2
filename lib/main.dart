cf4db1e4d7e25241b4e3447e45c35b122cf8920c
jinkijack <tiagomc123@gmail.com>
2023-10-31 01:42:59 +0000

    layout5

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,89 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Hello World',
+      debugShowCheckedModeBanner: false,
+      title: 'Imagem Flutter',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'MEU APP'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isTextVisible = false;
+
+  void _toggleTextVisibility() {
+    setState(() {
+      isTextVisible = !isTextVisible;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Image.network(
-            'https://i.imgur.com/fzgwYzq.png',
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
+        title: const Text(
+          'MEU APP',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
           ),
-          Positioned(
-            top: 20.0,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: const Text('Clique para Prosseguir'),
+        ),
+        backgroundColor: Colors.lightBlue,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _toggleTextVisibility,
+            child: const Text(
+              'Clique para proseguir',
+              style: TextStyle(
+                color: Colors.blue,
+              ),
             ),
           ),
+          Image.network(
+            'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+            width: 650,
+            height: 450,
+            fit: BoxFit.cover,
+          ),
+          if (isTextVisible)
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                color: Colors.white,
+                padding: const EdgeInsets.all(16.0),
+                child: const Text(
+                  'Este é o Mascote do Flutter',
+                  style: TextStyle(
+                    color: Colors.lightBlue,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
         ],
       ),
     );
