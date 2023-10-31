989dbb4c96b56c2b08683527ed5740a04a29981f
jinkijack <tiagomc123@gmail.com>
2023-10-31 01:34:59 +0000

    layout2

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'interface 1',
+      title: 'Flutter Hello World',
       theme: ThemeData(
-        scaffoldBackgroundColor: const Color.fromARGB(255, 227, 201, 201),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xFFEF9A9A),
-        ),
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final String title;
+
+  const MyHomePage({super.key, required this.title});
+
+  // Função para abrir um URL no navegador
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('MEU APP'),
-        centerTitle: true,
+        title: Text(title),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      drawer: Drawer(
+        child: ListView(
           children: <Widget>[
-            Text(
-              'Corpo do Texto',
+            const UserAccountsDrawerHeader(
+              accountName: Text('Tiago Mateus Carvalho'),
+              accountEmail: Text('tiagomc123@hotmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Text(
+                  'TC',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.article_outlined),
+              title: const Text('Curso de Angular'),
+              onTap: () {
+                _launchURL('https://www.youtube.com/watch?v=vJt_K1bFUeA&t=1s');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.article_outlined),
+              title: const Text('Curso de Java'),
+              onTap: () {
+                _launchURL('https://www.youtube.com/watch?v=V5I11DRE-1M');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.article_outlined),
+              title: const Text('Curso de IA generativa'),
+              onTap: () {
+                _launchURL('https://www.youtube.com/watch?v=gozr-2677-0');
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: const Color(0xFFEF9A9A),
-        child: const Icon(Icons.add),
+      body: const Center(
+        child: Text(
+          'Hello, World!',
+        ),
       ),
     );
   }
